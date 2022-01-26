df93bdaf0d059777f73cdf61240c79aaf6fcab8b
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2022-01-26 19:12:26 +0530

    Add styling to card components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,10 +2,13 @@ import React from "react";
 
 function Card(props){
     return(
-        <div>
-            <img src={props.imageSrc} alt={props.title}/>
-            <h1>{props.title}</h1>
-            <p>{props.content}</p>
+        <div className="row">
+            <div className={ "card " + props.title}>
+                <img src={props.imageSrc} alt={props.title}/>
+                <h1>{props.title}</h1>
+                <p>{props.content}</p>
+                <a className="btn">Learn More</a>
+            </div>
         </div>
     );
 }
