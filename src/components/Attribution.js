2eccfe85b64483820a1990f665321b80e9019dce
Naveen Kumar Ongole <naveen.ongole@gmail.com>
2022-02-17 07:28:26 +0530

    add coded by name and ref

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -4,7 +4,7 @@ function Attribution(){
     return(
         <div className="attribution">
             Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Your Name Here</a>.
+            Coded by <a href="https://github.com/Naveen39O">Ongole Naveen</a>.
         </div>
     )
 }
